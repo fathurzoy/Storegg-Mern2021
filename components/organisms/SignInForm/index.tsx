b7886fd910567343d07a9ac68d0d0d2ffc49305c
fathurzoy <fathurrahmanf123@gmail.com>
2021-09-04 19:20:25 +0700

    Integrasi Sign-In Page

--- components/organisms/SignInForm/index.tsx
+++ components/organisms/SignInForm/index.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { setLogin } from "../../../services/auth";
+import { useRouter } from "next/dist/client/router";
 
 const SignInForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const onSubmit = async () => {
+    const data = {
+      email: email,
+      password: password,
+    };
+    // console.log("data", data);
+
+    if (!email || !password) {
+      toast.error("Email dan Password wajib diisi!!");
+    } else {
+      const response = await setLogin(data);
+      if (response.error) {
+        toast.error(response.message);
+      } else {
+        toast.success("Login Berhasil");
+        router.push("/");
+      }
+    }
+  };
+
   return (
     <>
       <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign In</h2>
@@ -21,6 +50,8 @@ const SignInForm = () => {
           name="email"
           aria-describedby="email"
           placeholder="Enter your email address"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </div>
       <div className="pt-30">
@@ -37,27 +68,31 @@ const SignInForm = () => {
           name="password"
           aria-describedby="password"
           placeholder="Your password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <div className="button-group d-flex flex-column mx-auto pt-50">
-        <a
+        <button
+          type="button"
           className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
-          href="../index.html"
-          role="button"
+          onClick={onSubmit}
         >
           Continue to Sign In
-        </a>
+        </button>
         {/* <!-- <button type="submit"
-                                className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
-                                role="button">Continue to Sign In</button> --> */}
-        <a
-          className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill"
-          href="/sign-up"
-          role="button"
-        >
-          Sign Up
-        </a>
+            className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
+            role="button">Continue to Sign In</button> --> */}
+        <Link href="/sign-up">
+          <a
+            className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill"
+            role="button"
+          >
+            Sign Up
+          </a>
+        </Link>
       </div>
+      <ToastContainer />
     </>
   );
 };
