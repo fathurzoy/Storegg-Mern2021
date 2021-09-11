d05f86d70a1d4d8606a7791d442dbf0656923180
fathurzoy <fathurrahmanf123@gmail.com>
2021-09-11 20:24:29 +0700

    Setup Basic SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Navbar from "../components/organisms/Navbar";
 import Reached from "../components/organisms/Reached";
 import Story from "../components/organisms/Story";
 import TransactionStep from "../components/organisms/TransactionStep";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   useEffect(() => {
@@ -16,6 +17,23 @@ const Home: NextPage = () => {
 
   return (
     <>
+      <Head>
+        <title>StoreGG - Get a New Experience in Gaming</title>
+        <meta
+          name="description"
+          content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati"
+        />
+        <meta
+          property="og:title"
+          content="StoreGG - Get a New Experience in Gaming"
+        />
+        <meta
+          property="og:description"
+          content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati"
+        />
+        <meta property="og:image" content="https://imageurlkalian" />
+        <meta property="og:url" content="https://storegg.com" />
+      </Head>
       {/* <!-- Navbar --> */}
       <Navbar />
 
